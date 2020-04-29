9906e07ef9c8c1d36ea8752a40f6a4eb223630f6
Jake Riesser <Jake.Riesser@Gmail.com>
2020-04-29 12:11:35 -0400

    first working version - opted for individual image loads on each card rather than stitching and uploading to imgur

--- app.py
+++ app.py
@@ -28,9 +28,12 @@ __maintainer__ = "Jake.Riesser@Gmail.com"
 __email__ = "Jake.Riesser@Gmail.com"
 __status__ = "Development"
 
+
 class Error(Exception):
     """Base class for exceptions in this module."""
+
     pass
+
 
 class FormattingError(Error):
     """Exception raised for errors in the input text file.
@@ -41,6 +44,7 @@ class FormattingError(Error):
 
     def __init__(self, message):
         self.message = message
+
 
 class CardNotFoundError(Error):
     """Exception raised when a card is not found on YGOPro.
@@ -53,79 +57,32 @@ class CardNotFoundError(Error):
         self.cardName = cardName
         self.message = "Cannot find the card {0} on YGoPro".format(self.cardName)
 
-class TTSDeckbuilder():
-    '''All the main deckbuilding logic goes here'''
-    def __init__(self, filename, logger, imgurClientId):
+
+class TTSDeckbuilder:
+    """All the main deckbuilding logic goes here"""
+
+    def __init__(self, filename, logger):
         self._logger = logger
         self._filename = filename
-        self._imgurClientId = imgurClientId
+        # self._imgurClientId = imgurClientId
         self._imgurUploadUrl = "https://api.imgur.com/3/upload"
 
-    # region TTS serialization objects 
-    # This is a bit anti-python, but meh, old habits die hard and it'll make the serialzation easier.
-    class ObjectStateTransform():
-        '''TTS ObjectStateTransform class for serialzation'''
-        def __init__(self):
-            self.posX = 0
-            self.posY = 0
-            self.posZ = 0
-            self.rotY = 180
-            self.rotZ = 180
-            self.scaleX = 1
-            self.scaleY = 1
-            self.scaleZ = 1
+    class Card:
+        """represents and stores information about a single card"""
 
-    class ContainedObjectTransform(ObjectStateTransform):
-        '''TTS ContainedObjectTransform class for serialization'''
-        def print(self):
-            pass
-
-    class ContainedObject():
-        '''TTS ContainedObject class for serialization'''
-        def __init__(self):
-            self.Name = ""
-            self.NickName = ""
-            self.ContainedObjectTransform = TTSDeckbuilder.ContainedObjectTransform()
-            self.CardID = -1
-
-    class DeckInfo():
-        '''TTS DeckInfo class for serialization'''
-        def __init__(self):
-            self.NumWidth = 0
-            self.NumHeight = 0
-            self.FaceURL = ""
-            self.BackURL = "https://i.imgur.com/ubLQ2p2.jpg"
-
-    class CustomDeck():
-        '''TTS CustomDeck class for serialization'''
-        def __init__(self):
-            self.one = TTSDeckbuilder.DeckInfo() # how many decks pop out. TODO - add extra and side deck parsing
-
-    class ObjectState():
-        '''TTS ObjectState class for serialization'''
-        def __init__(self):
-            self.Transform = TTSDeckbuilder.ObjectStateTransform()
-            self.Name = ""
-            self.ContainedObjects = []
-            self.DeckIDs = []
-            self.CustomDeck = TTSDeckbuilder.CustomDeck()
-
-    # endregion
-
-    class Card():
-        '''represents and stores information about a single card'''
         def __init__(self, cardName):
             self._cardName = cardName
             self._metaDataUrl = "https://db.ygoprodeck.com/api/v7/cardinfo.php?name="
             self._ygoproId = -1
+            self._deckID = -1
             self._desc = ""
             self._imageURL = ""
 
             self.populateDataFromYGoPro()
 
         def populateDataFromYGoPro(self):
-            '''Gets metadata from YGoPro.org for a given cardname'''
-            
+            """Gets metadata from YGoPro.org for a given cardname"""
+
             r = requests.get(fix_text(self._metaDataUrl + self._cardName.strip()))
             r.raise_for_status()
             responseJson = r.json()
@@ -134,27 +91,30 @@ class TTSDeckbuilder():
 
             else:
                 # populate some useful class info from the respose
-                responseData = responseJson.get("data")[0] # going for exact names here
+                responseData = responseJson.get("data")[0]  # going for exact names here
                 self._ygoproId = responseData.get("id")
                 self._desc = responseData.get("desc")
-                self._imageURL = responseData.get("card_images")[0].get("image_url") # first printing, TODO - add option to randomize or specify printing
+                self._imageURL = responseData.get("card_images")[0].get(
+                    "image_url"
+                )  # first printing, TODO - add option to randomize or specify printing
 
         def print(self):
-            return json.dumps([self._cardName, self._ygoproId, self._desc, self._imageURL])
-
+            return json.dumps(
+                [self._cardName, self._ygoproId, self._desc, self._imageURL]
+            )
 
     def build(self):
         expandedDecklist = self.expandDecklistFromFile(args.filename, logging)
         deckWithData = self.getDeckData(expandedDecklist)
-        image = self.createDeckImage(deckWithData)
-        self.uploadImageToImgur(image)
+        self.assignDeckIDs(deckWithData)
+        # image = self.createDeckImage(deckWithData)
+        # self.uploadImageToImgur(image)
         self.createTTSFile(deckWithData)
 
-    
     def expandDecklistFromFile(self, filename, logging):
-        '''attempts to parse and return a list of expanded cardnames from the specified txt file'''
+        """attempts to parse and return a list of expanded cardnames from the specified txt file"""
         self._logger.info("Expanding decklist from text file {0}...".format(filename))
-        
+
         results = []
         with open(filename) as file:
             lines = list(file)
@@ -169,16 +129,18 @@ class TTSDeckbuilder():
                         while j > 0:
                             results.append(fix_text(cardName))
                             self._logger.debug("Added card {0}".format(cardName))
-                            j-=1
+                            j -= 1
                     else:
                         # formatting error
-                        raise FormattingError(message="Unexpected format: {0}".format(line))    
+                        raise FormattingError(
+                            message="Unexpected format: {0}".format(line)
+                        )
         return results
 
     def getDeckData(self, expandedDecklist):
-        '''Gets a json representation of the deck with information from ygopro.com'''
+        """Gets a json representation of the deck with information from ygopro.com"""
         self._logger.info("Creating deck metadata...")
-            
+
         results = []
         for cardName in expandedDecklist:
             card = self.Card(cardName)
@@ -187,16 +149,28 @@ class TTSDeckbuilder():
 
         return results
 
+    def assignDeckIDs(self, deck):
+        """assigns a unique deckID for TTS use. Like cards should have the same ID"""
+        ctr = 100  # increments by 100s
+        for card in deck:
+            if card._deckID == -1:
+                card._deckID = ctr
+                Id = card._ygoproId
+                for c in deck:  # all copies of the card should have the same deckID
+                    if c._ygoproId == Id:
+                        c._deckID = ctr
+                ctr += 100
+
     def createDeckImage(self, deckWithMetaData):
-        '''creates an image to upload to imgur or store locally based on the cards in the deck'''
+        """creates an image to upload to imgur or store locally based on the cards in the deck"""
         self._logger.info("Generating deck image, this may take a hot sec...")
-        
+
         images = []
         for card in deckWithMetaData:
             r = requests.get(card._imageURL, stream=True)
             r.raise_for_status()
             r.raw.decode_content = True
-            images.append(r.raw) # store the bytes
+            images.append(r.raw)  # store the bytes
 
         images = [Image.open(x) for x in images]
 
@@ -205,53 +179,131 @@ class TTSDeckbuilder():
         totalWidth = sum(widths)
         maxHeight = max(heights)
 
-        result = Image.new('RGB', (totalWidth, maxHeight))
+        result = Image.new("RGB", (totalWidth, maxHeight))
 
         x_offset = 0
         for im in images:
-            result.paste(im, (x_offset,0))
+            result.paste(im, (x_offset, 0))
             x_offset += im.size[0]
 
-        result.save('test.jpg')
+        result.save("test.jpg")
 
         return result
 
-    def uploadImageToImgur(self, image):
-        '''utilizes the imgur API to upload the image so it's publically available'''
-        self._logger.info("Uploading image to imgur...")
-        headers = {"Authorization" : "Client " + self._imgurClientId}
-        data = {"image" : image}
-        r = requests.post(self._imgurUploadUrl, data, headers=headers)
-        r.raise_for_status()
+    # def uploadImageToImgur(self, image):
+    #     '''utilizes the imgur API to upload the image so it's publically available'''
+    #     self._logger.info("Uploading image to imgur...")
+    #     headers = {"Authorization" : "Client " + self._imgurClientId}
+    #     data = {"image" : image}
+    #     r = requests.post(self._imgurUploadUrl, data, headers=headers)
+    #     r.raise_for_status()
 
     def createTTSFile(self, deck):
-        pass
+        """generates a json file and saves it to disk for importing into TTS
+        ObjectStates
+            [{
+                Name:
+                ContainedObjects:
+                [{
+                    {CardID, Name, NickName, Transform} < -- each card needs one of these
+                }],
+                DeckIDs: [], < -- deck distrubution (100 = 1 below, increment by 100s)
+                CustomDeck: { < -- card data
+                    1 : {
+                        FaceUrl, 
+                        BackUrl,
+                        NumHeight,
+                        NumWidth,
+                        BackIsHidden : True
+                    },
+                    Transform
+                }
+            }]
+        
+        """
+        Transform = {
+            "posX": 0,
+            "posY": 0,
+            "posZ": 0,
+            "rotX": 0,
+            "rotY": 180,
+            "rotZ": 180,
+            "scaleX": 1,
+            "scaleY": 1,
+            "scaleZ": 1,
+        }
+
+        TTSDeck = []
+        for card in deck:
+            TTSDeck.append(
+                {
+                    "CardID": card._deckID,
+                    "Name": "Card",
+                    "Nickname": card._cardName,
+                    "Transform": Transform
+                }
+            )
+
+        DeckIDs = []
+        for card in deck:
+            DeckIDs.append(card._deckID)
+
+        CustomDeck = {}
+        for card in deck:
+            if str(int(card._deckID / 100)) not in CustomDeck.keys():
+                CustomDeck[str(int(card._deckID / 100))] = {
+                    "FaceURL": card._imageURL,
+                    "BackURL": "https://i.imgur.com/ubLQ2p2.jpg",
+                    "NumHeight": 1,
+                    "NumWidth": 1,
+                    "BackIsHidden": True,
+                }
+
+        # construct the json object
+        TTS = {}  # base object
+        ObjectStates = []
+        ObjectStates.append(
+            {
+                "Name": "DeckCustom",
+                "ContainedObjects": TTSDeck,
+                "DeckIDs": DeckIDs,
+                "CustomDeck": CustomDeck,
+                "Transform": Transform,
+            }
+        )
+        TTS["ObjectStates"] = ObjectStates
+        self._logger.debug(json.dumps(TTS))
+        with open('deck.json', 'w') as outFile:
+            json.dump(TTS, outFile)
+
 
 def main(args, logLevel):
     logging.basicConfig(format="%(levelname)s: %(message)s", level=logLevel)
-    builder = TTSDeckbuilder(args.filename, logging, args.imgurClientId)
+    builder = TTSDeckbuilder(args.filename, logging)
     builder.build()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-    description='A console tool for generating a Tabletop Simulator (TTS) Deck from a text file')
+        description="A console tool for generating a Tabletop Simulator (TTS) Deck from a text file"
+    )
 
     # Positional Arguments
-    parser.add_argument('filename',
-                        help="The relative path to the text file containing the desired decklist. Each line in the " +
-                            "file must be in the form of: 'N Cardname', where 'N' is the number of copies " +
-                            "of the card to include")
-    
-    parser.add_argument('imgurClientId',
-                        help="In order to upload images to imgur's API, you need to register your app here: https://api.imgur.com/oauth2/addclient, and feed me your clientID (or ask Jake nicely for his)")
+    parser.add_argument(
+        "filename",
+        help="The relative path to the text file containing the desired decklist. Each line in the "
+        + "file must be in the form of: 'N Cardname', where 'N' is the number of copies "
+        + "of the card to include",
+    )
+
+    # parser.add_argument('imgurClientId',
+    #                     help="In order to upload images to imgur's API, you need to register your app here: https://api.imgur.com/oauth2/addclient, and feed me your clientID (or ask Jake nicely for his)")
 
     parser.add_argument(
-                      "-v",
-                      "--verbose",
-                      help="increase output verbosity",
-                      action="store_true")
+        "-v", "--verbose", help="increase output verbosity", action="store_true"
+    )
     args = parser.parse_args()
-    
+
     # Setup logging
     if args.verbose:
         logLevel = logging.DEBUG
